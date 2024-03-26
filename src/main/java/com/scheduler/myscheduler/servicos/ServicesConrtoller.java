7bdabf01b7d095ad7596e0c44dcda30f15d14811
Oliver <oliverperboni@gmail.com>
2024-03-26 12:27:00 +0000

    cors config

--- src/main/java/com/scheduler/myscheduler/servicos/ServicesConrtoller.java
+++ src/main/java/com/scheduler/myscheduler/servicos/ServicesConrtoller.java
@@ -3,6 +3,7 @@ package com.scheduler.myscheduler.servicos;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +25,7 @@ public class ServicesConrtoller {
         this.servicesService = servicesService;
     }
 
-
+    @CrossOrigin
     @GetMapping
     public List<Servicos> getServices(){
         return servicesService.getServices();
